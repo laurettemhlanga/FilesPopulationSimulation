c4af52faae83eae0695200e739f557a8d94dc782
Laurette Mhlanga <laurette@debmower_ajd>
2019-02-22 10:10:54 +0000

    testing how verstile delta is?

--- files/calling_script_PopulationSimulation.R
+++ files/calling_script_PopulationSimulation.R
@@ -7,16 +7,24 @@ library(PopulationSimulation)
 
 birth_count <- birth_counts_fun(total_births = 1000,
                                 birth_dates = 1945:1950,
-                                delta = 1,
+                                delta = 0.5,
                                 birthrate = birth_rate_fun)
 
-incidence_m <- incidence_matrix_fun(age_steps = 3, birth_dates = 0:5, incidence_fun)
+incidence_m <- incidence_matrix_fun(age_steps = 3, 
+                                    birth_dates = 0:5, 
+                                    generate_incidence_fun = incidence_fun, 
+                                    delta = 0.5)
 
-base_mortality_m <- base_mortality_matrix_fun(3, 0:5, generate_base_mortality_fun = base_mortality_fun)
+
+base_mortality_m <- base_mortality_matrix_fun(3, 
+                                              0:5, 
+                                              delta = 0.5, 
+                                              generate_base_mortality_fun = base_mortality_fun)
+
 
 survival_prob <- susceptible_cumulative_survival_fun(incidence_matrix = incidence_m,
                                                         base_mortality_matrix = base_mortality_m,
-                                                        delta = 1)
+                                                        delta = 0.5)
 
 susceptible_pop_counts <- susceptible_population_fun(cumulative_survival_matrix = survival_prob,
                                                 birth_counts = birth_count)
@@ -24,7 +32,14 @@ susceptible_pop_counts <- susceptible_population_fun(cumulative_survival_matrix 
 
 infected_survival_probs <- infected_mortality_array_fun (age_steps = 3, birth_dates = 1945:1950,
                                                      excess_mortality = excess_mortality_fun,
-                                                     base_mortality = base_mortality_fun)
+                                                     base_mortality = base_mortality_fun,
+                                                     delta = 0.5)
+
+transformed <- transform_data(age_max = 3,
+                              data_to_transform = susceptible_pop_counts,
+                              date_of_birth = 1945:1950,
+                              delta = 0.5)
+
 
 infected <- infected_population_fun(susceptible_pop_counts = susceptible_pop_counts,
                                     incidence_prob = incidence_m,
