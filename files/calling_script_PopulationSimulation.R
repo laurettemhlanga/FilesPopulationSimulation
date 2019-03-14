8c2cd00ae9f37a0ec0354d80637227dc39afb366
Laurette Mhlanga <laurette@debmower_ajd>
2019-03-14 09:28:04 +0000

    adding the function do one simulation

--- files/calling_script_PopulationSimulation.R
+++ files/calling_script_PopulationSimulation.R
@@ -1,26 +1,58 @@
-
-setwd("H:/FilesPopulationSimulation")
-files.sources = list.files("H:/PopulationSimulation/R")
+setwd("/home/laurette/Desktop/Github/PopulationSimulation/R")
+files.sources = list.files("/home/laurette/Desktop/Github/PopulationSimulation/R")
 sapply(files.sources, source)
 
-list_of_times = seq(t_1, t_2 , delta)
 
 
-birth_count <- birth_counts_fun(list_of_times,
-                                delta = 0.5,
-                                birthrate = constant_birth_rate_fun)
 
 
-incidence_m <- incidence_matrix_fun(age_steps = 3,
-                                    birth_dates = 0:5,
-                                    generate_incidence_fun = constant_tent_incidence_fun,
-                                    delta = 0.5)
+
+Do_one_simulation <- function(first_birth_time = 1985, last_birth_time = 1990, time_step = 1, max_age = 3, 
+                              birth_rate = constant_birth_rate, base_mortality = time_indep_age_linear_base_mortality, 
+                              incidence = time_indept_age_tent_incidence,
+                              excess_mortality = excess_mortality_fun)
+  {
+  
+  #wrapper function to the functions in Population simulation project. 
+  
+  list_of_times <- seq(first_birth_time, last_birth_time, time_step )
+  
+  birth_count <- birth_counts(dates_needing_birth_counts = list_of_times,  
+                              birth_rate = birth_rate, time_step = time_step)
+  
+  
+  incidence_m <- incidence_matrix(max_age, list_of_times, incidence, time_step)
+  
+  
+  base_mortality_m <- base_mortality_matrix(max_age, list_of_times, base_mortality, time_step)
+  
+  
+  
+  survival_prob <- susceptible_cumulative_survival(incidence_matrix = incidence_m, base_mortality_matrix = base_mortality_m,
+                                                       time_step)
+  
+  
+  susceptible_pop_counts <- susceptible_population(cumulative_survival_matrix = survival_prob,
+                                                   birth_counts = birth_count)
+  
+  return(susceptible_pop_counts)
+  
+  
+}
+
+
+
+
+# setwd("/home/laurette/Desktop/Github/PopulationSimulation/R")
+# files.sources = list.files("/home/laurette/Desktop/Github/PopulationSimulation/R")
+# sapply(files.sources, source)
+
 
 
 base_mortality_m <- base_mortality_matrix_fun(3,
                                               0:5,
                                               delta = 0.5,
-                                              generate_base_mortality_fun =constant_base_mortality_fun)
+                                              generate_base_mortality_fun = constant_base_mortality_fun)
 
 
 
@@ -36,7 +68,7 @@ survival_prob <- susceptible_cumulative_survival_fun(incidence_matrix = incidenc
 
 
 
-#survival_prob * birth_count
+
 
 
 
@@ -51,10 +83,12 @@ susceptible_pop_counts <- susceptible_population(cumulative_survival_matrix = su
 
 
 
-
-infected_0 <- incidence_matrix_exp(incidence_matrix = transform_data(incidence_m),
-                          base_mortality_matrix = transform_data(base_mortality_m),
+infected_0 <- incidence_matrix_exp(incidence_matrix = incidence_m,
+                          base_mortality_matrix = base_mortality_m,
                           delta = 0.5) * susceptible_pop_counts
+
+
+
 
 
 infected_survival_probs <- infected_mortality_array_fun (age_steps = 3, birth_dates = 0:5,
