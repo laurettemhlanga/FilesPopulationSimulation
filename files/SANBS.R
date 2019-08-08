0c15767e058159ebe5dd81367eccb03da161302d
laurette13 <laurettemhlanga13@gmail.com>
2019-08-08 08:56:14 +0200

    adding SANBS script

--- files/SANBS.R
+++ files/SANBS.R
@@ -1,51 +1,81 @@
-#blood donors 
+#blood donors
+
+rm(list = ls())
+
+
 library(PopulationSimulation)
 library(inctools)
 
 prevalence <- function(default = T,
-                       constant,
+                       constant = 0.02,
                        gradient,
                        time){
+
+  if (isTRUE(default)){
+
+    prevalence <- rep(constant, length(time))
+
+  }else{
+
+    prevalence <- constant + (gradient * time)
+
+  }
+  return(prevalence)
+}
+
+
+
+prevalence_recency <- function(default = T,
+                               constant = 0.05,
+                               gradient,
+                               time){
   
   if (isTRUE(default)){
     
     prevalence <- rep(constant, length(time))
     
   }else{
-  
+    
     prevalence <- constant + (gradient * time)
-  
+    
   }
   return(prevalence)
-} 
-
-#prevalence(constant = 0.001, time  = 1:5)
-
-
-
-
-prevalence_recency <- function( prevalence_donors,
-                                probability_testing_receny){
-  
-  prevalencerecency <- probability_testing_receny * prevalence_donors
-  
-  return(prevalencerecency)
 }
 
 
 
 
 
-dataset <- data.frame(donation_time = 1:20, 
-                      donors = rep(100000, 20))
+
+prevalence_dataset <- function(donationtimes = seq(1,365, 0.25), #0.0025
+                               prevalence_func = prevalence,
+                               prevalences_recency_func = prevalence_recency
+                               ){
+  
+  #takes in the donationdates, donorpopulation, prevalences, and prevalencerecency. outputs a dataframe of input vectors
+  #
+
+  prevalencedataset <- data.frame(donationtimes = donationtimes)
+
+  for (times in seq_along(donationtimes)){
+  
+  prevalencedataset$HIV_status[times] = sample(x = c(1,0),size = 1, prob = c(prevalence_func(time = donationtimes[times]),
+                                                                      1 - prevalence_func(time = donationtimes[times])))
+  
+  prevalencedataset$HIVrecency_status[times] = ifelse(prevalencedataset$HIV_status[times] = 1, 
+                                                      sample(x = c(1,0), 
+                                                             size = 1,
+                                                             prob = c(prevalences_recency_func(time = prevalencedataset$donationtimes[times]),
+                                                                      1 - prevalences_recency_func(time = prevalencedataset$donationtimes[times]))), 0)
+                                                      
+  
+  }
+  
+  
+  return(prevalencedataset)
+}
 
 
-dataset$prevalence <- prevalence(constant = 0.0166, time  = 1:20)
-
-
-dataset$prevalence <- prevalence_recency(prevalence_donors = prevalence(constant = 0.0166, time  = 1:20),
-                                         probability_testing_receny = probability_of_recently_infected(time_in_years = 5,
-                                                                                                       type = "step"))
 
 
 
@@ -58,8 +88,20 @@ dataset$prevalence <- prevalence_recency(prevalence_donors = prevalence(constant
 
 
 
-MDRI <- 271
-FRR <- 0.002
+
+# MDRI <- function(func, 
+#                  lower, 
+#                  upper,
+#                  type,...){
+# #theoratical mean duration of recent infection, wrapper function to R integrate and 
+# #returns the 
+#   mdri <- integrate(func,
+#                     lower = lower, 
+#                     upper = upper, type = type)$value
+#   return(mdri)
+# }
+
+
 
 incprops(PrevH = 0.2, RSE_PrevH = 0.028, PrevR = 0.1, RSE_PrevR = 0.094, BS_Count = 10000, Boot = F,
          alpha = 0.05, MDRI = 200, RSE_MDRI = 0.05, FRR = 0.01, RSE_FRR = 0.2,
